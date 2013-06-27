c640d4499172cdc26360bd4b6312dbd51ea43bd4
mikansoncho <mikansoncho333@gmail.com>
2013-06-27 13:54:33 +0900

    Add loop.php

--- wp-content/themes/mikanblog/loop.php
+++ wp-content/themes/mikanblog/loop.php
@@ -1,14 +1,13 @@
 <?php
-
+if ( have_posts() ) : while ( have_posts() ) : the_post();
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <title></title>
-    </head>
-    <body>
-        
-    </body>
-</html>
+ 
+<div class="post">
+	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+	<?php the_content(); ?>
+</div>
+ 
+<?php
+endwhile;
+endif;
+?>
